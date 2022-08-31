ed2a7771140fd0062f78470548d2a5aba491cef2
bobmagicii <bob@majdak.net>
2022-08-31 20:30:29 +0000

    prepare for testing

--- phpunit-coverage.sh
+++ phpunit-coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-XDEBUG_MODE=coverage phpunit tests --coverage-html /opt/web-new/www/_object
+XDEBUG_MODE=coverage phpunit tests --coverage-html /opt/web-new/www/_surface
---
+++ tests/Nether/EngineTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nether\Surface\Test;
+use PHPUnit;
+
+class EngineTest
+extends PHPUnit\Framework\TestCase {
+
+	/** @test */
+	public function
+	BasicTest():
+	void {
+
+		$this->AssertTrue(TRUE);
+		return;
+	}
+
+}
