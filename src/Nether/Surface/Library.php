043d8d0b9f33f4ea2deafc8f6ca42a68faaeed50
bobmagicii <bob@majdak.net>
2022-12-14 07:22:28 +0000

    thusfar

--- src/Nether/Surface/Library.php
+++ src/Nether/Surface/Library.php
@@ -3,47 +3,24 @@
 namespace Nether\Surface;
 use Nether;
 
-use SplFileInfo;
-use Nether\Object\Datastore;
-
 class Library
 extends Nether\Common\Library {
 
 	public const
-	ConfThemes       = 'Nether.Surface.Themes',
-	ConfThemeRoot    = 'Nether.Surface.Theme.Root';
+	ConfThemes    = 'Nether.Surface.Themes',
+	ConfThemeRoot = 'Nether.Surface.Theme.Root';
 
-	static public function
-	Init(...$Argv):
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	public function
+	OnLoad(...$Argv):
 	void {
 
-		static::OnInit(...$Argv);
-
-		return;
-	}
-
-	static public function
-	InitDefaultConfig(?Datastore $Config=NULL):
-	Datastore {
-
-		parent::InitDefaultConfig($Config);
-
-		$Config->BlendRight([
-			static::ConfThemeRoot    => './themes',
-			static::ConfThemes       => [ 'local', 'default' ]
+		static::$Config->BlendRight([
+			static::ConfThemeRoot => './themes',
+			static::ConfThemes    => [ 'local', 'default' ]
 		]);
-
-		return $Config;
-	}
-
-	////////////////////////////////////////////////////////////////
-	////////////////////////////////////////////////////////////////
-
-	static public function
-	OnInit(Datastore $Config=NULL, ...$Argv):
-	void {
-
-		static::InitDefaultConfig($Config);
 
 		return;
 	}
