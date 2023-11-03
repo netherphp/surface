978d957a15670a6cd28e9b70ea724ba5297a75f8
bob <bob@tech.live>
2023-11-03 01:28:38 +0000

    thusfar

--- src/Nether/Surface/Util.php
+++ src/Nether/Surface/Util.php
@@ -7,20 +7,13 @@ class Util {
 	static public function
 	MakePathableKey(string $Input):
 	string {
-	/*//
-	utility method i have in almost all of my projects to take input uris and
-	spit out versions that would break anything if we tried to use it in a
-	file path. so its a super sanitiser only allowing alphas, numerics,
-	dashes, periods, and forward slashes. does not allow dot stacking
-	to prevent traversal foolery.
-	//*/
 
 		$Output = strtolower(trim($Input));
 
 		// allow things that could be nice clean file names.
 
 		$Output = preg_replace(
-			'#[^a-zA-Z0-9\-\/\.]#', '',
+			'#[^a-zA-Z0-9\-\/\._]#', '',
 			$Output
 		);
 
